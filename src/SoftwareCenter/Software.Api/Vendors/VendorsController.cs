08ebe01626cf87bfb3e551eb93d53db7087ce255
Jeff Gonzalez <jeff@hypertheory.com>
2025-08-12 12:20:53 -0400

    before lunch

--- src/SoftwareCenter/Software.Api/Vendors/VendorsController.cs
+++ src/SoftwareCenter/Software.Api/Vendors/VendorsController.cs
@@ -4,12 +4,22 @@ namespace Software.Api.Vendors;
 
 public class VendorsController : ControllerBase
 {
-    [HttpPost("/vendors")]
+    [HttpPost("/vendors")] // POST to a collection resource. 
     public async Task<ActionResult> AddAVendorAsync(
         [FromBody] VendorCreateModel request,
-        CancellationToken token)
+        [FromServices] ICreateVendors vendorCreator
+        )
     {
-        return Ok(request);
+        // Validation
+        // Validate the incoming request, if it isn't valid, send a 400 response.
+        // If it is valid - "store this thing" "Side effect"
+        // return a success result, and if you can, a copy of the thing you "created"
+
+   
+        VendorDetailsModel response = await vendorCreator.CreateVendorAsync(request);
+
+        // consider a 201 status code - which "created" - and a copy of the thing you just created.
+        return Ok(response); // Bogus.
     }
 }
 
@@ -25,3 +35,5 @@ public class VendorsController : ControllerBase
 
 public record PointOfContact(string Name, string Email, string Phone);
 public record VendorCreateModel(string Name, string Url, PointOfContact Contact);
+
+public record VendorDetailsModel(Guid Id, string Name, string Url, PointOfContact Contact, string CreatedBy, DateTimeOffset CreatedOn);
--- src/SoftwareCenter/Software.Tests/Vendors/ManagersCanAddVendors.cs
+++ src/SoftwareCenter/Software.Tests/Vendors/ManagersCanAddVendors.cs
@@ -1,7 +1,8 @@
 ﻿
 using System.Net.Http.Json;
 using Alba;
-
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Software.Api.Vendors;
 
 namespace Software.Tests.Vendors;
@@ -16,7 +17,13 @@ public class ManagersCanAddVendors
         var vendorToPost = new VendorCreateModel("Microsoft", "https://www.microsoft.com", new PointOfContact("Satya", "satya@microsoft.com", "800 the-boss"));
 
 
-        var host = await AlbaHost.For<Program>();
+        var host = await AlbaHost.For<Program>(config =>
+        {
+            config.ConfigureTestServices(services =>
+            {
+                services.AddScoped<ICreateVendors, StubbedVendorCreator>();
+            });
+        });
 
         var postResponse = await host.Scenario(api =>
         {
@@ -24,11 +31,26 @@ public class ManagersCanAddVendors
             api.StatusCodeShouldBeOk();
         });
 
-        var postResponseBody = await postResponse.ReadAsJsonAsync<VendorCreateModel>();
+        var postResponseBody = await postResponse.ReadAsJsonAsync<VendorDetailsModel>();
         Assert.NotNull(postResponseBody);
-        Assert.Equal(vendorToPost, postResponseBody);
+        // Does it have an ID.
+        Assert.Equal(Guid.Parse("c904c645-ed16-49a6-b3f6-e32e8662c400"), postResponseBody.Id);
+        Assert.Equal("sue@aol.com", postResponseBody.CreatedBy);
+        /// etc. etc.
+        /// 
+
         // query the database and see if the thing was saved.
 
         // when I do a GET /vendors, this new vendor shows up there.
+    }
+}
+
+
+public class StubbedVendorCreator : ICreateVendors
+{
+    public Task<VendorDetailsModel> CreateVendorAsync(VendorCreateModel request)
+    {
+        var fake = new VendorDetailsModel(Guid.Parse("c904c645-ed16-49a6-b3f6-e32e8662c400"), request.Name, request.Url, request.Contact, "sue@aol.com", DateTimeOffset.UtcNow);
+        return Task.FromResult(fake);
     }
 }
